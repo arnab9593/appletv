6a3c03953dc6c0300020205dbb877566ac890664
Janaki Kangalgouda <42041306+janakigouda@users.noreply.github.com>
2022-06-18 12:41:48 +0530

    version 102 release

--- singinpage/login.js
+++ singinpage/login.js
@@ -1,75 +1,51 @@
-// // all  login js code here
-//   let     Data=JSON.parse(localStorage.getItem("")) || []
-//   console.log(Data)
-//   var fun =document.querySelector("a");
-//   fun.addEventListener("click",function(event){
-//     event.preventDefault();
-//     let form=document.getElementById("arrowIcon")
-//     var Dat={
-//       // AppleId:UserName.value,
-//      passwords:form.psd.value,
-//     };
-//    console.log(Dat);
-//     if((Passwords=="") || (passwords.length<8)){
-//       alert("non")
-// }
-
-
-//   });
-
-
-
-
-//*************************************************************** */
-
-// let   userData=JSON.parse(localStorage.getItem("setData")) || [];
+let userDetail = JSON.parse(localStorage.getItem("setData"));
 
 document.getElementById("a").addEventListener("click", function () {
-
-
-
-
 
   let ram = document.getElementById("UserName").value
   if (ram == "") {
     alert("enter email id")
   }
-
-
-
   else {
 
+    let ram = document.getElementById("UserName").value;
+
     let id = document.getElementById("arrowIcon")
-    id.addEventListener("click",function(){
-      loginFun();
+    id.addEventListener("click", function () {
+      let pass = ash.value;
+      loginFun(pass, ram);
     })
 
     id.style.marginLeft = "360px", marginTop = "-10px"
-    // let minus=document.createElement("input").value
 
-    // minus.setAttribute("id","ama")
-
-    // minus.placeholder="Password"
     let ash = document.getElementById("pas")
+    ash.setAttribute("placeholder", "XXXXXXXXXXX");
     ash.style.border = "2px solid grey"
-    console.log(ash.value)
-    // minus.style.fontSize="22px"
+    // console.log(ash.value);
     document.getElementById("pass").append(ash, id)
 
     document.getElementById("create").style.marginTop = "146px"
-    // if(minus==""){
-    //   alert("yygyvdfftdbn")
-    // }
-
-
 
   }
 
-})
-    //   let tp=document.getElementById("pas")
-    //   document.getElementById("pass").append(id,tp)
-    //   document.getElementById("a").style.marginTop="50px",marginLeft="360px"
+});
 
-    function loginFun(){
-      location.href="../index.html";
-    }
+
+function loginFun(password, name) {
+  if (name == "" || password == "") {
+    alert("Enter password!!");
+  } else {
+    userDetail.forEach(element => {
+      if (password == element.password && name == element.email) {
+        let name = element.username + " " + element.lastname;
+        sessionStorage.setItem("userName", name);
+        alert("LOgin successfull...");
+        location.href = "../index.html";
+      } else {
+        window.location.reload();
+        alert("Email or password is wrong!!!! , Please recheck once again ....");
+      }
+    });
+  }
+  // console.log(password, name)
+}
